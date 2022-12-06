ef2f4b83025c25fe4e3f09dd7c9c449c2a0b59bf
hp <hp@pop-os.localdomain>
2022-12-06 19:43:28 -0300

    feat: feito os testes 12

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,9 +1,23 @@
 from src.pre_built.brazilian_jobs import read_brazilian_file
 
+BRAZILIAN_FILE_PATH = "tests/mocks/brazilians_jobs.csv"
+
 
 def test_brazilian_jobs():
-    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0] == {
-        "title": "Maquinista",
-        "salary": "2000",
-        "type": "trainee",
-    }
+    assert isinstance(read_brazilian_file(BRAZILIAN_FILE_PATH), list)
+    assert isinstance(read_brazilian_file(BRAZILIAN_FILE_PATH)[0], dict)
+    assert len(read_brazilian_file(BRAZILIAN_FILE_PATH)) == 15
+    test_brazilian_jobs_rename_keys()
+    test_brazilian_jobs_removed_keys()
+
+
+def test_brazilian_jobs_rename_keys():
+    assert "title" in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
+    assert "salary" in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
+    assert "type" in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
+
+
+def test_brazilian_jobs_removed_keys():
+    assert "titulo" not in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
+    assert "salario" not in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
+    assert "tipo" not in read_brazilian_file(BRAZILIAN_FILE_PATH)[0]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,21 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+job1 = {"min_salary": 1000, "max_salary": 2000, "date_posted": "2019-01-01"}
+job2 = {"min_salary": 2000, "max_salary": 3000, "date_posted": "2019-01-02"}
+job3 = {"min_salary": 3000, "max_salary": 4000, "date_posted": "2019-01-03"}
+job4 = {"min_salary": 5000, "max_salary": "", "date_posted": "2019-01-05"}
+job5 = {"min_salary": 6000, "max_salary": 5000, "date_posted": ""}
+job6 = {"min_salary": "", "max_salary": 6000, "date_posted": "2019-01-04"}
+
+job_mock_min = [job1, job2, job3, job4, job5, job6]
+job_mock_max = [job1, job2, job3, job4, job5, job6]
+job_mock_date = [job1, job2, job3, job4, job5, job6]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(job_mock_min, "min_salary")
+    assert job_mock_min == [job1, job2, job3, job4, job5, job6]
+    sort_by(job_mock_max, "max_salary")
+    assert job_mock_max == [job6, job5, job3, job2, job1, job4]
+    sort_by(job_mock_date, "date_posted")
+    assert job_mock_date == [job4, job6, job3, job2, job1, job5]
