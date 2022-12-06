2e9ea635502dcd292f483d210262065a77bf96bc
hp <hp@pop-os.localdomain>
2022-12-06 18:05:54 -0300

    feat: testes do count_ocorrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+
+
+def test_counter2():
+    assert count_ocurrences("data/jobs.csv", "Java") == 676
+
+
+def test_counter3():
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("data/jobs.json", "javascript")
