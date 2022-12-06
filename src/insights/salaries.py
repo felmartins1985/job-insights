278f93e619b4b8b6ebb03674e9216e60dd86bf09
hp <hp@pop-os.localdomain>
2022-12-06 17:31:10 -0300

    feat: funcao para avaliar a profissao com o salario desejado

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -53,29 +53,18 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("Job doesn't have min_salary or max_salary")
+    if (
+        not str(job["min_salary"]).isdigit()
+        or not str(job["max_salary"]).isdigit()
+    ):
+        raise ValueError("Job min_salary or max_salary isn't a valid integer")
+    if int(job["min_salary"]) > int(job["max_salary"]):
+        raise ValueError("Job min_salary is greather than max_salary")
+    if not str(salary).lstrip("-").isdigit():
+        raise ValueError("Salary isn't a valid integer")
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
